dc9ecfa1b2888934a61dc9e5b9143304dd7bbfa4
diabetrix <alulusha333@gmail.com>
2023-03-13 11:29:22 +0500

    bebra2

--- bebra/Form1.Designer.cs
+++ bebra/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -37,7 +39,7 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(244, 192);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(225, 35);
+            this.button1.Size = new System.Drawing.Size(225, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "Bebroid -> bebra";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,11 +56,34 @@
             this.label1.Text = "Bebroid";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(321, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "biba";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(244, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "biba - boba";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -72,6 +97,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- bebra/Form1.cs
+++ bebra/Form1.cs
@@ -26,5 +26,10 @@ namespace bebra
         {
             label1.Text = "bebra";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "boba";
+        }
     }
 }
